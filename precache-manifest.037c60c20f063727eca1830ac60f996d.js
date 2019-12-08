bfa06fd157848f6cd4a67e695e4f0fa5b5b20874
Telmo Sampaio <telmoasampaio@gmail.com>
2019-12-08 17:05:12 +0000

    Updates

---
+++ precache-manifest.037c60c20f063727eca1830ac60f996d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e9be0f5cd187396f6ae74878b64d2c",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "c2b0f7293df20f7a475a",
+    "url": "/react-deploy/static/css/main.bf92f223.chunk.css"
+  },
+  {
+    "revision": "be282bae84d9dd2b6237",
+    "url": "/react-deploy/static/js/2.649e0b91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deploy/static/js/2.649e0b91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2b0f7293df20f7a475a",
+    "url": "/react-deploy/static/js/main.05c25c21.chunk.js"
+  },
+  {
+    "revision": "1d020b3c296b9ed426af",
+    "url": "/react-deploy/static/js/runtime-main.3475fb37.js"
+  },
+  {
+    "revision": "8d963fa8ec386990f39af915902486bd",
+    "url": "/react-deploy/static/media/telmo-pic.8d963fa8.jpg"
+  }
+]);
