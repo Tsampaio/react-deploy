f48f1edab0d9f016158b6727ced45f0e999bfae0
Telmo Sampaio <telmoasampaio@gmail.com>
2019-12-08 17:04:06 +0000

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './img/telmo-pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Myu React App</h1>
+      <h2>Author: Telmo Sampaio</h2>
+      <img src={profileImage} alt="profile-image" />
     </div>
   );
 }
